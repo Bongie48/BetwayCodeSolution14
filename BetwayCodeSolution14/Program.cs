2c4a97d249f3ccaabe5e8c78bcb1ab74a721d333
Nombongo Gxekiwe <s223326534@mandela.ac.za>
2025-02-07 23:24:27 +0200

    Add project files.

---
+++ BetwayCodeSolution14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetwayCodeSolution14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long power = 132651201231;
+            string result = IsMagicalPotion(power) ? "YES" : "NO";
+            Console.WriteLine(result);
+            Console.ReadLine(); 
+        }
+        static bool IsMagicalPotion(long power)
+        {
+            if (power < 1)
+            {
+                return false;
+            }
+            double cubeRoot = Math.Round(Math.Pow(power, 1.0 / 3));
+            return Math.Pow(cubeRoot, 3) == power;
+        }
+    }
+}
---
+++ BetwayCodeSolutions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetwayCodeSolutions
+{
+    //Question 14 : Magic Potion Identifier
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long power = 132651201231;
+            string result = IsMagicalPotion(power) ? "YES" : "NO";
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        static bool IsMagicalPotion(long power)
+        {
+            if (power < 1)
+            {
+                return false; 
+            }
+            double cubeRoot = Math.Round(Math.Pow(power, 1.0 / 3));
+            return Math.Pow(cubeRoot, 3) == power;
+        }
+
+    }
+}
